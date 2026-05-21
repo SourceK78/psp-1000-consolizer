52ae43ae9e791111fe00e422c388ed4e99051126
SourceK78 <sourcek.dev@gmail.com>
2026-05-21 19:07:42 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# PSP-1000 HDMI Consolizer with PS2 Controller
+
+This project is a PSP-1000 Consolizer project that incorporates the "HISPEEDIDO PSP-1000 HDMI mod" to enable operation with a PS2 controller.  
+The original shell case allows for PSP-1000 gameplay with both UMD drive functionality and HDMI output.  
+
+## Build
+
+The following parts are required to build the Consolizer.  
+These parts assume the use of a [shell case](#shell-case) I designed.  
+
+### Parts needed
+- [main PCB](#pcb)
+- [HISPEEDIDO PSP-1000 HDMI mod](https://hispeedidostore.com/ja/products/for-psp-1000-ips-screen-high-brightness-psp1000-digital-hdmi-to-hdmi-play-games-on-tv)
+- [HDMI micro Male Connector D3](https://aliexpress.com/item/1005006437300837.html)
+- [HDMI female Connector A4](https://aliexpress.com/item/1005006437300837.html)
+- [HDMI FFC 15cm](https://aliexpress.com/item/1005006437300837.html)
+- [KCD11 Rocker Switch](https://aliexpress.com/item/4000973563250.html)
+- [KY-016 RGB LED Module](https://aliexpress.com/item/1005006238275322.html)
+- [Tact Switch Module](https://aliexpress.com/item/1005011664637597.html)
+- [JST PH2.0 2Pin Connector and Wire](https://aliexpress.com/item/1005009087160808.html) * for PSP Power Supply / Power Switch
+- [JST PH2.0 4Pin Connector and Wire](https://aliexpress.com/item/1005009087160808.html) * for LED module / PSP Analog Input
+- [FFC 10pin 0.5mm Pitch B-Type(Reverse Direction)](https://aliexpress.com/item/1005007653265454.html) * for PSP Digital Input
+- [FFC 24pin 0.5mm Pitch B-Type(Reverse Direction)](https://aliexpress.com/item/1005007653265454.html) * for PSP Digital Input
+- [Nylon Flat Washer M2x5x1mm](https://aliexpress.com/item/33021883302.html)
+- [Phillips head pan head screw M2x5mm](https://aliexpress.com/item/1005002364189187.html) * for PCB / LED Module
+- [Phillips head pan head screw M2x10mm](https://aliexpress.com/item/1005002364189187.html) * for Shell Case
+- PSP AC Adapter (Official versions are recommended)
+
+## PCB
+
+It is possible to create circuit boards using services such as JLCPCB and PCBWAY by utilizing the data in the [./pcb](./pcb) folder.  
+
+### BOM
+| **Reference** | **Part** | **Link** |
+|---------|------|------|
+|U1 | M5Stamp S3A (1.27mm Pitch ) | - |
+|U2 | MCP23017 (SOIC-28) | - |
+|U3 | AD5142 (TSSOP-16) | - |
+|C1, C5, C7 | 10 uF capacitor (1206) | - |
+|C2, C3, C4, C6, C8 | 0.1 uF capacitor (1206) | - |
+|C9 | 1 uF capacitor (1206) | - |
+|C10 | 47 uF capacitor (1206) | - |
+|CN1, CN5 | JST PH2.0 4Pin Connector B4B-PH-K-S | - |
+|CN2, CN3, CN4, CN6, CN7 | JST PH2.0 2Pin Connector PHB2B-PH-K-S | - |
+|D1 | Schottky Diodes SS14 | - |
+|FPC1 | FFC connector with 24 pins, 0.5mm pitch, bottom contacts | - |
+|FPC2 | FFC connector with 10 pins, 0.5mm pitch, bottom contacts | - |
+|JOY1 | PS2 Controller Connector (90 degrees Female) | [aliexpress](https://aliexpress.com/item/1005006039721141.html) |
+|Q1 | Nch-MOSFET BSS138 (SOT-23) | - |
+|R1, R2, R3 | 4.7 kOhm resistor (1206) | - |
+|R4, R8, R9 | 1 kOhm resistor (1206) | - |
+|R5 | 10 kOhm resistor (1206) | - |
+|R6, R7 | 1 MOhm resistor (1206) | - |
+
+## Shell Case
+
+You can download the STL file from the link below and create a dock case for your PCB using a 3D printer.  
+If you don't have a 3D printer, you can also commission JLC3DP or PCBWAY to make one for you.  
+https://www.printables.com/model/1728475-psp-1000-consolizer-shell-case  
+
+## Acknowledgments
+
+This project is made possible by HISPEEDIDO's HDMI mod and SukkoPera's PsxNewLib.  
+
+## License
+
+This project is is be distributed under the same GPL-3.0 license as PSxNewLib.  
---
+++ psp-1000-control/psp-1000-control.ino
@@ -0,0 +1,449 @@
+/*******************************************************************************
+ * This file is part of PsxNewLib.                                             *
+ * Edited by SourceK for PSP-1000 Consolizer.                                  *
+ *                                                                             *
+ * Copyright (C) 2019-2020 by SukkoPera <software@sukkology.net>               *
+ *                                                                             *
+ * PsxNewLib is free software: you can redistribute it and/or                  *
+ * modify it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * PsxNewLib is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with PsxNewLib. If not, see http://www.gnu.org/licenses.              *
+ *******************************************************************************
+ *
+ * This sketch will dump to serial whatever is done on a PSX controller. It is
+ * an excellent way to test that all buttons/sticks are read correctly.
+ *
+ * It's missing support for analog buttons, that will come in the future.
+ *
+ * This example drives the controller by bitbanging the protocol, there is
+ * another similar one using the hardware SPI support.
+ */
+
+#include <DigitalIO.h>
+#include <PsxControllerBitBang.h>
+#include <Wire.h>
+#include <SPI.h>
+
+#include <pgmspace.h>
+
+typedef const byte* PGM_BYTES_P;
+#define PSTR_TO_F(s) reinterpret_cast<const __FlashStringHelper *> (s)
+
+// Right analog stick center and deadzone settings
+const byte ANALOG_CENTER   = 128;
+const byte ANALOG_DEADZONE = 90;
+
+// PSX controller (BitBang)
+const byte PIN_PS2_DAT = 6;
+const byte PIN_PS2_CMD = 7;
+const byte PIN_PS2_ATT = 8;
+const byte PIN_PS2_CLK = 9;
+
+// I2C for MCP23017
+const byte PIN_I2C_SDA = 13;
+const byte PIN_I2C_SCL = 12;
+
+// MCP23017
+const uint8_t MCP23017_ADDR = 0x20;
+
+// MCP23017 output bit for PSP HOME button (R3 position = bit 15)
+// Adjust if your hardware uses a different output pin
+const uint8_t HOME_BIT = 15;
+
+// SPI for AD5142
+const byte PIN_SPI_SCK  = 2;
+const byte PIN_SPI_MOSI = 3;
+const byte PIN_SPI_MISO = 4;
+const byte PIN_SPI_SS   = 5;
+
+// AD5142 Command (16-bit: [CMD5:CMD0=000001][A1:A0] | data)
+const uint8_t CMD_RDAC1 = 0x10;  // 00000100: WRITE RDAC, address=0
+const uint8_t CMD_RDAC2 = 0x11;  // 00000101: WRITE RDAC, address=1
+
+// POWER LED (input) and RGB LED (PWM output)
+const byte POWER_LED = 1;
+const byte LED_R     = 39;
+const byte LED_G     = 40;
+const byte LED_B     = 41;
+
+// MODE Select
+const byte VIDEO_SCALE = 14;
+
+int powerState = -1;
+
+const char buttonSelectName[] PROGMEM = "Select";
+const char buttonL3Name[] PROGMEM = "L3";
+const char buttonR3Name[] PROGMEM = "R3";
+const char buttonStartName[] PROGMEM = "Start";
+const char buttonUpName[] PROGMEM = "Up";
+const char buttonRightName[] PROGMEM = "Right";
+const char buttonDownName[] PROGMEM = "Down";
+const char buttonLeftName[] PROGMEM = "Left";
+const char buttonL2Name[] PROGMEM = "L2";
+const char buttonR2Name[] PROGMEM = "R2";
+const char buttonL1Name[] PROGMEM = "L1";
+const char buttonR1Name[] PROGMEM = "R1";
+const char buttonTriangleName[] PROGMEM = "Triangle";
+const char buttonCircleName[] PROGMEM = "Circle";
+const char buttonCrossName[] PROGMEM = "Cross";
+const char buttonSquareName[] PROGMEM = "Square";
+
+const char* const psxButtonNames[PSX_BUTTONS_NO] PROGMEM = {
+	buttonSelectName,
+	buttonL3Name,
+	buttonR3Name,
+	buttonStartName,
+	buttonUpName,
+	buttonRightName,
+	buttonDownName,
+	buttonLeftName,
+	buttonL2Name,
+	buttonR2Name,
+	buttonL1Name,
+	buttonR1Name,
+	buttonTriangleName,
+	buttonCircleName,
+	buttonCrossName,
+	buttonSquareName
+};
+
+const char ctrlTypeUnknown[] PROGMEM = "Unknown";
+const char ctrlTypeDualShock[] PROGMEM = "Dual Shock";
+const char ctrlTypeDsWireless[] PROGMEM = "Dual Shock Wireless";
+const char ctrlTypeGuitHero[] PROGMEM = "Guitar Hero";
+const char ctrlTypeOutOfBounds[] PROGMEM = "(Out of bounds)";
+
+const char* const controllerTypeStrings[PSCTRL_MAX + 1] PROGMEM = {
+	ctrlTypeUnknown,
+	ctrlTypeDualShock,
+	ctrlTypeDsWireless,
+	ctrlTypeGuitHero,
+	ctrlTypeOutOfBounds
+};
+
+PsxControllerBitBang<PIN_PS2_ATT, PIN_PS2_CMD, PIN_PS2_DAT, PIN_PS2_CLK> psx;
+
+boolean haveController = false;
+bool analogDpadEnabled = false;
+
+
+
+void writeButtons(uint16_t value) {
+	Wire.beginTransmission(MCP23017_ADDR);
+	Wire.write(0x14);  // OLATA
+	Wire.write(value & 0xFF);        // Lower 8bit
+	Wire.write((value >> 8) & 0xFF); // Upper 8bit
+	Wire.endTransmission();
+}
+
+// Remap PSX button word to desired output bit order for MCP23017:
+// out bit 0:Right, 1:Down, 2:Up, 3:Left, 4:L1, 5:Triangle, 6:Circle, 7:R1,
+//         8:Square, 9:Cross, 10:Start, 11:Select, 12:L3, 13:R2, 14:L2, 15:R3
+uint16_t remapButtons(uint16_t b) {
+	uint16_t out = 0;
+	if (b & (1 <<  5)) out |= (1 <<  0); // Right
+	if (b & (1 <<  6)) out |= (1 <<  1); // Down
+	if (b & (1 <<  4)) out |= (1 <<  2); // Up
+	if (b & (1 <<  7)) out |= (1 <<  3); // Left
+	if (b & (1 << 10)) out |= (1 <<  4); // L1
+	if (b & (1 << 12)) out |= (1 <<  5); // Triangle
+	if (b & (1 << 13)) out |= (1 <<  6); // Circle
+	if (b & (1 << 11)) out |= (1 <<  7); // R1
+	if (b & (1 << 15)) out |= (1 <<  8); // Square
+	if (b & (1 << 14)) out |= (1 <<  9); // Cross
+	if (b & (1 <<  3)) out |= (1 << 10); // Start
+	if (b & (1 <<  0)) out |= (1 << 11); // Select
+	if (b & (1 <<  1)) out |= (1 << 12); // L3
+	if (b & (1 <<  9)) out |= (1 << 13); // R2
+	if (b & (1 <<  8)) out |= (1 << 14); // L2
+	if (b & (1 <<  2)) out |= (1 << 15); // R3
+	return out;
+}
+
+void writePot(uint8_t pot, uint8_t value) {
+	uint8_t command = (pot == 0) ? CMD_RDAC1 : CMD_RDAC2;
+
+	SPI.beginTransaction(SPISettings(500000, MSBFIRST, SPI_MODE1));
+	digitalWrite(PIN_SPI_SS, LOW);
+	SPI.transfer(command);
+	SPI.transfer(map(value, 0, 255, 255, 0)); // Inverse Value
+	digitalWrite(PIN_SPI_SS, HIGH);
+	SPI.endTransaction();
+}
+
+byte psxButtonToIndex (PsxButtons psxButtons) {
+	byte i;
+
+	for (i = 0; i < PSX_BUTTONS_NO; ++i) {
+		if (psxButtons & 0x01) {
+			break;
+		}
+
+		psxButtons >>= 1U;
+	}
+
+	return i;
+}
+
+void dumpButtons (PsxButtons psxButtons) {
+	static PsxButtons lastB = 0;
+
+	if (psxButtons != lastB) {
+		lastB = psxButtons;     // Save it before we alter it
+
+		Serial.print (F("Pressed: "));
+
+		for (byte i = 0; i < PSX_BUTTONS_NO; ++i) {
+			byte b = psxButtonToIndex (psxButtons);
+			if (b < PSX_BUTTONS_NO) {
+				PGM_BYTES_P bName = reinterpret_cast<PGM_BYTES_P> (pgm_read_ptr (&(psxButtonNames[b])));
+				Serial.print (PSTR_TO_F (bName));
+			}
+
+			psxButtons &= ~(1 << b);
+
+			if (psxButtons != 0) {
+				Serial.print (F(", "));
+			}
+		}
+
+		Serial.println ();
+	}
+}
+
+void dumpAnalog (const char *str, const byte x, const byte y) {
+	Serial.print (str);
+	Serial.print (F(" analog: x = "));
+	Serial.print (x);
+	Serial.print (F(", y = "));
+	Serial.println (y);
+}
+
+void setup () {
+	delay (300);
+	Serial.begin (115200);
+	Serial.println (F("Ready!"));
+
+	Wire.begin(PIN_I2C_SDA, PIN_I2C_SCL);
+
+	pinMode(PIN_SPI_SS, OUTPUT);
+	digitalWrite(PIN_SPI_SS, HIGH);  // CS Initial HIGH
+	SPI.begin(PIN_SPI_SCK, PIN_SPI_MISO, PIN_SPI_MOSI, PIN_SPI_SS);
+
+	pinMode(POWER_LED, INPUT_PULLUP);
+	pinMode(LED_R, OUTPUT);
+	pinMode(LED_G, OUTPUT);
+	pinMode(LED_B, OUTPUT);
+
+	pinMode(VIDEO_SCALE, OUTPUT);
+	digitalWrite(VIDEO_SCALE, LOW);  // Mode Select Initial LOW
+
+	// // Set OLATA to full Low (0x00) Just in case...
+	// Wire.beginTransmission(MCP23017_ADDR);
+	// Wire.write(0x14);   // IODIRA
+	// Wire.write(0x00);   // All pin output
+	// Wire.endTransmission();
+
+	// // Set OLATB to full Low (0x00) Just in case...
+	// Wire.beginTransmission(MCP23017_ADDR);
+	// Wire.write(0x15);   // IODIRB
+	// Wire.write(0x00);   // All pin output
+	// Wire.endTransmission();
+	
+	// Set IODIRA to full output (0x00)
+	Wire.beginTransmission(MCP23017_ADDR);
+	Wire.write(0x00);   // IODIRA
+	Wire.write(0x00);   // All pin output
+	Wire.endTransmission();
+
+	// Set IODIRB to full output (0x00)
+	Wire.beginTransmission(MCP23017_ADDR);
+	Wire.write(0x01);   // IODIRB
+	Wire.write(0x00);   // All pin output
+	Wire.endTransmission();
+
+	// Initialize
+	writeButtons(0xFFFF);
+	writePot(0, 255);
+	writePot(1, 255);
+
+	analogWrite(LED_R, 64);
+	analogWrite(LED_G, 0);
+	analogWrite(LED_B, 0);
+}
+
+void loop () {
+	static byte slx, sly, srx, sry;
+
+	if (!haveController) {
+		if (psx.begin ()) {
+			Serial.println (F("Controller found!"));
+			delay (300);
+			if (!psx.enterConfigMode ()) {
+				Serial.println (F("Cannot enter config mode"));
+			} else {
+				PsxControllerType ctype = psx.getControllerType ();
+				PGM_BYTES_P cname = reinterpret_cast<PGM_BYTES_P> (pgm_read_ptr (&(controllerTypeStrings[ctype < PSCTRL_MAX ? static_cast<byte> (ctype) : PSCTRL_MAX])));
+				Serial.print (F("Controller Type is: "));
+				Serial.println (PSTR_TO_F (cname));
+
+				if (!psx.enableAnalogSticks ()) {
+					Serial.println (F("Cannot enable analog sticks"));
+				}
+
+				if (!psx.enableAnalogButtons ()) {
+					Serial.println (F("Cannot enable analog buttons"));
+				}
+
+				if (!psx.enableRumble (false)) {
+					Serial.println (F("Cannot disable rumble"));
+				}
+
+				if (!psx.exitConfigMode ()) {
+					Serial.println (F("Cannot exit config mode"));
+				}
+			}
+
+			haveController = true;
+		}
+	} else {
+		if (!psx.read ()) {
+			Serial.println (F("Controller lost :("));
+			haveController = false;
+			writeButtons(0xFFFF);
+			writePot(0, 255);
+			writePot(1, 255);
+			digitalWrite(VIDEO_SCALE, LOW);
+			slx = sly = srx = sry = 255;  // Force analog re-report on reconnect
+		} else {
+			uint8_t rx, ry;
+			psx.getRightAnalog (rx, ry);
+			if (rx != srx || ry != sry) {
+				dumpAnalog ("Right", rx, ry);
+				srx = rx;
+				sry = ry;
+			}
+
+			PsxButtons rawButtons = psx.getButtonWord ();
+			bool btnL2    = rawButtons & (1 << 8);  // L2
+			bool btnR2    = rawButtons & (1 << 9);  // R2
+			bool btnR3    = rawButtons & (1 << 2);  // R3
+			bool btnStart = rawButtons & (1 << 3);  // Start
+			bool btnRight = rawButtons & (1 << 5);  // Right
+			bool homePress        = btnStart && btnRight;  // START+RIGHT → HOME
+			bool analogToggleCombo = btnStart && btnR3;    // START+R3 → toggle analog D-pad
+
+			// Toggle analog D-pad on rising edge of START+R3
+			static bool lastAnalogToggle = false;
+			if (analogToggleCombo && !lastAnalogToggle) {
+				analogDpadEnabled = !analogDpadEnabled;
+			}
+			lastAnalogToggle = analogToggleCombo;
+
+			// Build direction bits from right analog stick
+			PsxButtons analogBits = 0;
+			if (analogDpadEnabled) {
+				if (ry < ANALOG_CENTER - ANALOG_DEADZONE) analogBits |= (1 << 4); // UP
+				if (rx > ANALOG_CENTER + ANALOG_DEADZONE) analogBits |= (1 << 5); // RIGHT
+				if (ry > ANALOG_CENTER + ANALOG_DEADZONE) analogBits |= (1 << 6); // DOWN
+				if (rx < ANALOG_CENTER - ANALOG_DEADZONE) analogBits |= (1 << 7); // LEFT
+			}
+
+			// Build filtered buttons for MCP23017:
+			// L2/R2 are removed from default pass-through (handled specially below)
+			PsxButtons filteredButtons = rawButtons & ~((1 << 8) | (1 << 9));
+
+			// Remove Start when used as modifier key (HOME, VOL+, VOL-, analog toggle)
+			if (homePress || analogToggleCombo || (btnStart && (btnL2 || btnR2))) {
+				filteredButtons &= ~(1 << 3);
+			}
+
+			// START+RIGHT → HOME: also suppress Right button
+			if (homePress) {
+				filteredButtons &= ~(1 << 5);
+			}
+
+			// START+R3 → analog toggle: also suppress R3 button
+			if (analogToggleCombo) {
+				filteredButtons &= ~(1 << 2);
+			}
+
+			// START+L2 → VOL- (restore L2 bit),  START+R2 → VOL+ (restore R2 bit)
+			if (btnStart && btnL2) filteredButtons |= (1 << 8);
+			if (btnStart && btnR2) filteredButtons |= (1 << 9);
+
+			filteredButtons |= analogBits;
+			dumpButtons (filteredButtons);
+
+			// Write to MCP23017: apply HOME button on HOME_BIT (active low)
+			uint16_t mcpOutput = ~remapButtons (filteredButtons);
+			if (homePress) {
+				mcpOutput &= ~(1 << HOME_BIT);
+			}
+			writeButtons (mcpOutput);
+
+			// VIDEO_SCALE: HIGH only when both Start (bit 3) and Left (bit 7) are pressed
+			digitalWrite(VIDEO_SCALE, ((filteredButtons & (1 << 3)) && (filteredButtons & (1 << 7))) ? HIGH : LOW);
+
+			// Left analog stick:
+			// L2       → -X (left),   Y = center   (when Start not held)
+			// R2       → +X (right),  Y = center   (when Start not held)
+			// L2+R2    → -Y (up),     X = center   (when Start not held)
+			// START+L2/R2 → VOL (handled above), use physical analog
+			uint8_t lx, ly;
+			psx.getLeftAnalog (lx, ly);
+
+			uint8_t outLx, outLy;
+			if (!btnStart && btnL2 && btnR2) {
+				outLx = ANALOG_CENTER;
+				outLy = 0;            // -Y (push down)
+			} else if (!btnStart && btnL2) {
+				outLx = 0;              // -X (push left)
+				outLy = ANALOG_CENTER;
+			} else if (!btnStart && btnR2) {
+				outLx = 255;            // +X (push right)
+				outLy = ANALOG_CENTER;
+			} else {
+				outLx = lx;
+				outLy = ly;
+			}
+
+			if (outLx != slx || outLy != sly) {
+				dumpAnalog ("Left", outLx, outLy);
+				writePot(0, outLx);
+				writePot(1, outLy);
+				slx = outLx;
+				sly = outLy;
+			}
+		}
+	}
+
+	// Get power state
+	int curPwr = digitalRead(POWER_LED);
+	if ( powerState != curPwr ) {
+		powerState = curPwr;
+
+		if ( powerState ) {
+			analogWrite(LED_R, 64);
+			analogWrite(LED_G, 0);
+			analogWrite(LED_B, 0);
+		} else {
+			analogWrite(LED_R, 0);
+			analogWrite(LED_G, 0);
+			analogWrite(LED_B, 64);
+		}
+
+		Serial.print (F("Power State: "));
+		Serial.println (curPwr);
+	}
+
+	delay (1000 / 60);
+}
